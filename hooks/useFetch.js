ff07f14a3bd39bab5e5ab328936172ff1802b1de
Zehcnas <izan.perezsanchez@gmail.com>
2021-02-27 19:35:58 +0100

    my first commit and 3 hooks

---
+++ hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+export const useFetch = ( url ) => {
+    const [state, setState] = useState({data: null, loading: true, error: null})
+
+    useEffect( () => {
+	setState({data: null, loading: true, error: null})
+
+	fetch( url )
+	.then(resp => resp.json())
+	.then(data => {
+	    setState({
+		data: data,
+		loading: false,
+		error: null
+	    })
+	})
+    }, [url])
+
+    return state
+}
